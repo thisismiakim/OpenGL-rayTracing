dad96ce82cc87022b1746b8e547161c17f9fa1d7
miakim <helloiammkim@gmail.com>
2023-07-19 23:20:13 +0900

    first commit

---
+++ include/graphicFunctions.h
@@ -0,0 +1,38 @@
+#ifndef GRAPHIC_FUNCTIONS_H_
+#define GRAPHIC_FUNCTIONS_H_
+
+#include <iostream>
+#include <SDL2/SDL.h>
+
+#include "SDLFunctions.h"
+#include "color.h"
+#include "graphic.h"
+#include "object.h"
+#include "app.h"
+
+
+// Clearing Renderer
+// : erasing everything that on the render and painting it all the pixels with a predefined colors
+// : it runs every frame before we actually draw a new state to the renderer
+static void ClearScreen(SDL_Renderer* renderer){
+    // set color
+    SDL_SetRenderDrawColor(renderer, Colors::BLACK.r, Colors::BLACK.g, Colors::BLACK.b,Colors::BLACK.a);
+    // select renderer
+    SDL_RenderClear(renderer);
+}
+
+
+// Drawing Function
+void DrawSquare(int x, int y)
+{
+    SDL_Rect rect;
+    rect.x = x - (app.square.WIDTH /2);
+    rect.y = y - (app.square.HEIGHT /2);
+    rect.w = app.square.WIDTH;
+    rect.h = app.square.HEIGHT;
+}
+
+
+
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,82 +1,32 @@
 #include <iostream>
+
+#define SDL_MAIN_HANDLED
 #include <SDL2/SDL.h>
 
-// Graphics
-const int WINDOW_WIDTH = 512;
-const int WINDOW_HEIGHT = 284;
-SDL_Window* g_main_window;
-SDL_Renderer* g_main_renderer;
-
-// Colors
-namespace Colors{
-    SDL_Color BLACK = {0, 0, 0, SDL_ALPHA_OPAQUE};
-}
+#include "SDLFunctions.h"
+#include "color.h"
+#include "graphic.h"
+#include "object.h"
+#include "app.h"
+#include "graphicFunctions.h"
+#include "appFunctions.h"
 
 
-//SDL Initialization Function
-static bool Init(){
-    if (SDL_Init(SDL_INIT_EVERYTHING) > 0 ){
-        std::cout << "SDL_Init failed with error: "<< SDL_GetError() << std::endl;
-        return EXIT_FAILURE;
-    }
 
-    g_main_window = SDL_CreateWindow(
-        "SDL WindowTitle",
-        SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED,
-        WINDOW_WIDTH,
-        WINDOW_HEIGHT,
-        SDL_WINDOW_OPENGL
-    );
-
-    if(g_main_window == nullptr){
-        std::cout << "Unable to create the main window. Error: " << SDL_GetError() << std::endl;
-        SDL_Quit();
-        return EXIT_FAILURE;
-    }
-
-    g_main_renderer = SDL_CreateRenderer(g_main_window, -1, SDL_RENDERER_PRESENTVSYNC);
-
-    return true;
-}
-
-// Shutdown Function, Clean Up
-void Shutdown(){
-    if (g_main_window != nullptr){
-        SDL_DestroyWindow(g_main_window);
-        g_main_window = nullptr;
-    }
-
-    if (g_main_renderer != nullptr){
-        SDL_DestroyRenderer(g_main_renderer);
-        g_main_renderer = nullptr;
-    }
-
-    SDL_Quit();
-}
-
-// Clearing Renderer
-// erasing everything that on the render and painting it all the pixels with a predefined colors
-// it runs every frame before we actually draw a new state to the renderer
-static void ClearScreen(SDL_Renderer* renderer){
-    // set color
-    SDL_SetRenderDrawColor(renderer, Colors::BLACK.r, Colors::BLACK.g, Colors::BLACK.b,Colors::BLACK.a);
-    // select renderer
-    SDL_RenderClear(renderer);
-}
-
-/* -----------------------------------------------------------------------*/
-// Main loop
-int main()
+int main(int argc, char *argv[])
 {
-    if (Init() == false) {Shutdown();}
+    if (InitApp() == false) 
+    {   
+        ShutdownApp();
+        return EXIT_FAILURE;
+    }
 
     // Draw loop
     SDL_Event event;
     bool running = true;
 
     while(running){
-        ClearScreen(g_main_renderer);
+        ClearScreen(app.render);
 
         // Check and process I/O events
         if (SDL_PollEvent(&event))
@@ -99,11 +49,11 @@ int main()
         }
 
         // Update the screen with the content rendered in the background buffer
-        SDL_RenderPresent(g_main_renderer);
+        SDL_RenderPresent(app.render);
 
     }
 
-    Shutdown();
+    ShutdownApp();
     return EXIT_SUCCESS;
 }
 
