f1abfef9a8c184ab0ff6b88adf6c5e0f194a8749
miakim <helloiammkim@gmail.com>
2023-07-22 22:27:39 +0900

    GLFW setting

--- Makefile
+++ Makefile
@@ -3,10 +3,10 @@ BUILD_DIR = build/debug
 CC = g++
 SRC_FILES = $(wildcard $(SRC_DIR)/*.cpp)
 OBJ_NAME = play
-INCLUDE_PATHS = -Iinclude
+INCLUDE_PATHS = -Iinclude -framework Cocoa -framework OpenGL -framework IOKit
 LIBRARY_PATHS = -Llib
 COMPILER_FLAGS = -std=c++11 -Wall -O0 -g
-LINKER_FLAGS = -lsdl2
+LINKER_FLAGS = -lsdl2 -lglfw
 
 all:
 	$(CC) $(COMPILER_FLAGS) $(LINKER_FLAGS) $(INCLUDE_PATHS) $(LIBRARY_PATHS) $(SRC_FILES) -o $(BUILD_DIR)/$(OBJ_NAME)
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
+#ifdef __APPLE__
+/* Defined before OpenGL and GLUT includes to avoid deprecation messages */
+#define GL_SILENCE_DEPRECATION
+#include <OpenGL/gl.h>
+#include <GLUT/glut.h>
+#else
+#include <GL/gl.h>
+#include <GL/glut.h>
+#endif
+
+
 #include <iostream>
 
 #define SDL_MAIN_HANDLED
 #include <SDL2/SDL.h>
+#include <GLFW/glfw3.h>
+
+
+
 
 #include "SDLFunctions.h"
 #include "color.h"
@@ -15,51 +30,85 @@
 
 int main(int argc, char *argv[])
 {
-    if (InitApp() == false) 
-    {   
-        ShutdownApp();
-        return EXIT_FAILURE;
+    // glfw test code
+    GLFWwindow* window;
+
+    /* init the library*/
+    if (!glfwInit())
+        return -1;
+    
+    /* create a windowed mode window and its OpenGL Context*/
+    window = glfwCreateWindow(640, 380, "Hello World", NULL, NULL);
+    if (!window){
+        glfwTerminate();
+        return -1;
+    }
+    /* Make the window's context current*/
+    glfwMakeContextCurrent(window);
+
+    /* loop until the user closes the window*/
+    while (!glfwWindowShouldClose(window)){
+        /*render here*/
+        glClear(GL_COLOR_BUFFER_BIT);
+        /*swap front and back buffers*/
+        glfwSwapBuffers(window);
+        /*poll for and process evenets*/
+        glfwPollEvents();
     }
 
-    // Draw loop
-    SDL_Event event;
-    bool running = true;
+    glfwTerminate();
+    return 0;
 
-    while(running){
-        ClearScreen(app.render);
 
-        // Check and process I/O events
-        // key events
-        if (SDL_PollEvent(&event))
-        {
-            switch (event.type)// 
-            {
-                case SDL_KEYDOWN:
-                {
-                    running = event.key.keysym.scancode != SDL_SCANCODE_ESCAPE;
-                    break;
-                }
-                case SDL_QUIT:
-                {
-                    running = false;
-                    break;
-                }
-                default: 
-                    break;
-            }
-        }
 
-        // Draw the new content on the renderer
-        DrawSquare(app.square.xpos, app.square.ypos);
+
+
+
+    // if (InitApp() == false) 
+    // {   
+    //     ShutdownApp();
+    //     return EXIT_FAILURE;
+    // }
+
+    // // Draw loop
+    // SDL_Event event;
+    // bool running = true;
+
+    // while(running){
+    //     ClearScreen(app.render);
+
+    //     // Check and process I/O events
+    //     // key events
+    //     if (SDL_PollEvent(&event))
+    //     {
+    //         switch (event.type)// 
+    //         {
+    //             case SDL_KEYDOWN:
+    //             {
+    //                 running = event.key.keysym.scancode != SDL_SCANCODE_ESCAPE;
+    //                 break;
+    //             }
+    //             case SDL_QUIT:
+    //             {
+    //                 running = false;
+    //                 break;
+    //             }
+    //             default: 
+    //                 break;
+    //         }
+    //     }
+
+    //     // Draw the new content on the renderer
+    //     DrawSquare(app.square.xpos, app.square.ypos);
         
 
 
-        // Update the screen with the content rendered in the background buffer
-        SDL_RenderPresent(app.render);
+    //     // Update the screen with the content rendered in the background buffer
+    //     SDL_RenderPresent(app.render);
 
-    }
+    // }
 
-    ShutdownApp();
+    // ShutdownApp();
     return EXIT_SUCCESS;
 }
 
